78f2de502608196fcbd7442f14567e0340171cbd
mac <murainoy@yahoo.com>
2019-11-23 02:56:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/kinwae/ContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kinwae
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
